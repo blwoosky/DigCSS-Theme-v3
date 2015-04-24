cdbe5048405376a0dec3b469c27e82368f215a4c
BlwooSky <blwoosky@gmail.com>
2015-04-24 15:57:37 +0800

    底部修改

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 	</div> <!-- END main body -->
-	<footer>
-	Designed By <a href="http://blwoosky.com" target="_blank">blwoosky</a>  <br/>
-Copyright &copy; 2013-<?php echo date("Y"); echo " "; bloginfo('name'); ?>
+	<footer class="text-center">
+		Designed By <a href="http://blwoosky.com" target="_blank">blwoosky</a><br/>
+		Copyright &copy; 2013-<?php echo date("Y"); echo " "; bloginfo('name'); ?>
 	</footer>
 	<?php wp_footer(); ?>
 </div><!-- END pageWrap -->
